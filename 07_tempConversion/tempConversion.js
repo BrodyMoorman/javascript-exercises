aa3adf8b184b8756fd578f00f5e0b1cc424dce2b
Brody Moorman <brodymoorman@outlook.com>
2022-10-07 10:18:49 -0400

    completed section 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  let newC = (tempF - 32 ) * (5/9);
+  newC = Math.round(newC*10)/10;
+  return newC;
 
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+  let newF = (tempC * (9/5)) + 32;
+  newF = Math.round(newF * 10)/10;
+  return newF;
 
 };
 
