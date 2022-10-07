d8f06afd8461aa2919dc3161f95f8dd37cda5dfa
Brody Moorman <brodymoorman@outlook.com>
2022-10-07 00:41:21 -0400

    Completed exercise 01-06

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let answer = "";
+    if(num < 0){
+        return "ERROR"
+    }
+    if(string === ''){
+        return ''
+    }
 
+    while(num > 0){
+        answer = answer + string;
+        num--
+
+    }
+    return answer;
+    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let bigger = 0;
+    let smaller = 0;
+    let total = 0;
+    if(first < 0 || second < 0){
+        return 'ERROR'
+    }
+    if(!Number.isFinite(first)||!Number.isFinite(second)){
+        return 'ERROR'
+    }
+    if(first > second){
+        bigger = first;
+        smaller = second;
+    }else{
+        bigger = second;
+        smaller = first;
+    }
+    
+    while(smaller <= bigger){
+         total += smaller;
+         smaller++;
+    }
+    return total;
 };
 
 // Do not edit below this line
